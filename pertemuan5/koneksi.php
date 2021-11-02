fe74ef6aef927bc9565ece3d15649cc791bfe2a3
Muhammad Ikhsan Nur Falah <62681631+kztscyth@users.noreply.github.com>
2021-11-02 21:40:40 +0700

    Add files via upload

---
+++ pertemuan5/koneksi.php
@@ -0,0 +1,12 @@
+<?php 
+    $host       = "localhost";
+    $user       = "root";
+    $password   = "";
+    $database   = "akademik_3";
+    $con    = mysqli_connect($host, $user, $password, $database);
+
+    if(!$con){
+        echo "Error".mysqli_connect_error();
+        exit();
+    }
+?>
